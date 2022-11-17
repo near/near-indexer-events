83f88ca9718899bed01a337120b4cbf2c1fa06c9
Roshaan Siddiqui <siddiqui.roshaan@gmail.com>
2022-11-17 22:19:18 +0100

    block processing metrics

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 // TODO cleanup imports in all the files in the end
+use crate::configs::Opts;
 use cached::SizedCache;
 use clap::Parser;
 use dotenv::dotenv;
 use futures::StreamExt;
-use std::env;
+use metrics_server::{init_metrics_server, BLOCK_PROCESSED_TOTAL, BLOCK_PROCESSING_TIME};
+use near_lake_framework::near_indexer_primitives;
+use std::{env, time::SystemTime};
 use tokio::{sync::Mutex, task};
 use tracing_subscriber::EnvFilter;
-use tracing_utils::DefaultSubcriberGuard;
-use metrics_server::init_metrics_server;
-
-use crate::configs::Opts;
-use near_lake_framework::near_indexer_primitives;
 mod configs;
 mod db_adapters;
+mod metrics_server;
 mod models;
 mod rpc_helpers;
 mod tracing_utils;
-mod metrics_server;
+#[macro_use]
+extern crate lazy_static;
 
 pub(crate) const LOGGING_PREFIX: &str = "indexer_events";
 
@@ -46,14 +46,18 @@ async fn main() -> anyhow::Result<()> {
         .blocks_preload_pool_size(100)
         .build()?;
 
-    let _writer_guard = init_tracing();
-    
+    let env_filter = EnvFilter::new("near_lake_framework=info,indexer_events=info");
+    let _subscriber = tracing_utils::init_tracing(env_filter).await;
+
     task::spawn_blocking(move || {
-        if let Err(val) = init_metrics_server() {
-            tracing::error!(target: LOGGING_PREFIX, "Error setting up the metrics server. -> {}", val)
+        if let Err(_val) = init_metrics_server() {
+            tracing::error!(
+                target: LOGGING_PREFIX,
+                "Error setting up the metrics server."
+            );
         }
     });
-    
+
     let (lake_handle, stream) = near_lake_framework::streamer(config);
     let json_rpc_client = near_jsonrpc_client::JsonRpcClient::connect(&opts.near_archival_rpc_url);
 
@@ -110,6 +114,8 @@ async fn handle_streamer_message(
     ft_balance_cache: &FtBalanceCache,
     contracts: &db_adapters::contracts::ContractsHelper,
 ) -> anyhow::Result<u64> {
+    let request_start = SystemTime::now();
+
     if streamer_message.block.header.height % 100 == 0 {
         tracing::info!(
             target: crate::LOGGING_PREFIX,
@@ -127,40 +133,8 @@ async fn handle_streamer_message(
         contracts,
     )
     .await?;
+
+    BLOCK_PROCESSED_TOTAL.inc();
+    BLOCK_PROCESSING_TIME.observe(request_start.elapsed().map_or(0.0, |d| d.as_secs_f64()));
     Ok(streamer_message.block.header.height)
-}
-
-fn init_tracing() -> DefaultSubcriberGuard {
-    let mut env_filter = EnvFilter::new("near_lake_framework=info,indexer_events=info");
-
-    if let Ok(rust_log) = env::var("RUST_LOG") {
-        if !rust_log.is_empty() {
-            for directive in rust_log.split(',').filter_map(|s| match s.parse() {
-                Ok(directive) => Some(directive),
-                Err(err) => {
-                    tracing::warn!(
-                        target: crate::LOGGING_PREFIX,
-                        "Ignoring directive `{}`: {}",
-                        s,
-                        err
-                    );
-                    None
-                }
-            }) {
-                env_filter = env_filter.add_directive(directive);
-            }
-        }
-    }
-
-    let (non_blocking_writer, _guard) = tracing_appender::non_blocking(std::io::stderr());
-
-    let subscriber = tracing_subscriber::FmtSubscriber::builder()
-        .with_env_filter(env_filter)
-        .with_writer(non_blocking_writer)
-        .finish();
-
-    DefaultSubcriberGuard {
-        subscriber_guard: tracing::subscriber::set_default(subscriber),
-        writer_guard: _guard,
-    }
 }
--- src/metrics_server.rs
+++ src/metrics_server.rs
@@ -3,52 +3,63 @@ use hyper::{
     service::{make_service_fn, service_fn},
     Body, Method, Request, Response, Server,
 };
-use opentelemetry::{
-    global,
-    metrics::{Counter, Histogram},
-    sdk::{
-        export::metrics::aggregation,
-        metrics::{controllers, processors, selectors},
-    },
-    Context, KeyValue,
-};
-use opentelemetry_prometheus::PrometheusExporter;
-use prometheus::{Encoder, TextEncoder};
+use prometheus::{Encoder, Histogram, HistogramOpts, IntCounter, Opts};
 use std::{convert::Infallible, env};
-use std::sync::Arc;
-use std::time::SystemTime;
 
-// lazy_static! {
-//     static ref HANDLER_ALL: [KeyValue; 1] = [KeyValue::new("handler", "all")];
-// }
+pub type Result<T, E> = std::result::Result<T, E>;
 
-async fn serve_req(
-    cx: Context,
-    req: Request<Body>,
-    state: Arc<AppState>,
-) -> Result<Response<Body>, hyper::Error> {
-    println!("Receiving request at path {}", req.uri());
-    let request_start = SystemTime::now();
+pub fn try_create_histogram(name: &str, help: &str) -> Result<Histogram, prometheus::Error> {
+    let opts = HistogramOpts::new(name, help);
+    let histogram = Histogram::with_opts(opts)?;
+    prometheus::register(Box::new(histogram.clone()))?;
+    Ok(histogram)
+}
 
-    state.curr_block_no_counter.add(&cx, 1, &[]);
+/// Attempts to crate an `IntCounter`, returning `Err` if the registry does not accept the counter
+/// (potentially due to naming conflict).
+pub fn try_create_int_counter(name: &str, help: &str) -> Result<IntCounter, prometheus::Error> {
+    let opts = Opts::new(name, help);
+    let counter = IntCounter::with_opts(opts)?;
+    prometheus::register(Box::new(counter.clone()))?;
+    Ok(counter)
+}
 
+lazy_static! {
+    pub static ref BLOCK_PROCESSED_TOTAL: IntCounter =
+        try_create_int_counter("total_blocks_processed", "Total number of blocks processed")
+            .unwrap();
+    pub static ref BLOCK_PROCESSING_TIME: Histogram =
+        try_create_histogram("block_processing_time", "Time spent indexing a block").unwrap();
+}
+
+async fn serve_req(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     let response = match (req.method(), req.uri().path()) {
         (&Method::GET, "/metrics") => {
-            let mut buffer = vec![];
-            let encoder = TextEncoder::new();
-            let metric_families = state.exporter.registry().gather();
-            encoder.encode(&metric_families, &mut buffer).unwrap();
-            // state.http_body_gauge.record(&cx, buffer.len() as u64, &[]);
+            let encoder = prometheus::TextEncoder::new();
+
+            let mut buffer = Vec::new();
+            if let Err(e) = encoder.encode(&prometheus::gather(), &mut buffer) {
+                eprintln!("could not encode custom metrics: {}", e);
+            };
+
+            let res = match String::from_utf8(buffer.clone()) {
+                Ok(v) => v,
+                Err(e) => {
+                    eprintln!("custom metrics could not be from_utf8'd: {}", e);
+                    String::default()
+                }
+            };
+            buffer.clear();
 
             Response::builder()
                 .status(200)
                 .header(CONTENT_TYPE, encoder.format_type())
-                .body(Body::from(buffer))
+                .body(Body::from(res))
                 .unwrap()
         }
         (&Method::GET, "/") => Response::builder()
             .status(200)
-            .body(Body::from("Hello World"))
+            .body(Body::from(""))
             .unwrap(),
         _ => Response::builder()
             .status(404)
@@ -56,74 +67,30 @@ async fn serve_req(
             .unwrap(),
     };
 
-        state.block_processing_duration_histogram.record(
-        &cx,
-        request_start.elapsed().map_or(0.0, |d| d.as_secs_f64()),
-        &[],
-    );
-   
     Ok(response)
 }
 
-struct AppState {
-    exporter: PrometheusExporter,
-    curr_block_no_counter: Counter<u64>,
-    block_processing_duration_histogram: Histogram<f64>,
-}
-
 #[tokio::main]
-pub async fn init_metrics_server() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    let controller = controllers::basic(
-        processors::factory(
-            selectors::simple::histogram([1.0, 2.0, 5.0, 10.0, 20.0, 50.0]),
-            aggregation::cumulative_temporality_selector(),
-        )
-        .with_memory(true),
-    )
-    .build();
-
-    let exporter = opentelemetry_prometheus::exporter(controller).init();
-    let cx = Context::new();
-
-    let meter = global::meter("ex.com/hyper");
-    let state = Arc::new(AppState {
-        exporter,
-        curr_block_no_counter: meter
-            .u64_counter("indexer.blocks_total")
-            .with_description("Total number of blocks processed.")
-            .init(),
-        block_processing_duration_histogram: meter
-            .f64_histogram("example.block_processing_duration_seconds")
-            .with_description("The time it takes each block to process.")
-            .init(),
-    });
-
+pub async fn init_metrics_server() -> std::result::Result<(), ()> {
     // For every connection, we must make a `Service` to handle all
     // incoming HTTP requests on said connection.
-    let make_svc = make_service_fn(move |_conn| {
-        let state = state.clone();
-        let cx = cx.clone();
-       
-        async move {
-            Ok::<_, Infallible>(service_fn(move |req| {
-                serve_req(cx.clone(), req, state.clone())
-            }))
-        }
+    let make_svc = make_service_fn(move |_conn| async move {
+        Ok::<_, Infallible>(service_fn(move |req| serve_req(req)))
     });
 
     let port: u16 = match env::var("PORT") {
-        Ok(val) => val.parse::<u16>().unwrap(), 
+        Ok(val) => val.parse::<u16>().unwrap(),
         _ => 3000,
     };
 
     let addr = ([127, 0, 0, 1], port).into();
-    
+
     let server = Server::bind(&addr).serve(make_svc);
 
     println!("Starting metrics server at http://{}", addr);
 
-    server.await?;
-
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
     Ok(())
 }
-
